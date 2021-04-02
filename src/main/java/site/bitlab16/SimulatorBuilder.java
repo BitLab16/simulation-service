286f08cde0e82f1fc9c2003e1c1ce8b74f011718
Samuele Vanini <samuele.vanini@live.it>
2021-04-02 10:52:21 +0200

    fix errore eccezione non disponibile nell'immagine docker

--- src/main/java/site/bitlab16/SimulatorBuilder.java
+++ src/main/java/site/bitlab16/SimulatorBuilder.java
@@ -1,15 +1,12 @@
 package site.bitlab16;
 
-import jdk.jshell.spi.ExecutionControl;
 import site.bitlab16.sources.SimulatedSource;
-
-import java.util.List;
 
 public interface SimulatorBuilder<T> {
     void reset();
     void setSimulatorType(SimulatorType type);
     void setSimulatedSource(SimulatedSource[] sources);
-    default void setOutput(T output) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("setOutput non e' definito per questo simulator");
+    default void setOutput(T output) throws Exception {
+        throw new Exception("setOutput non e' definito per questo simulator");
     };
 }
