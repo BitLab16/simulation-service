550e0f510e2b964dc36476ce7c0b8e1c45364c20
Samuele Vanini <samuele.vanini@live.it>
2021-04-02 10:23:38 +0200

    fix errore sullo sleep

--- src/main/java/site/bitlab16/kafka_producer/Consumer.java
+++ src/main/java/site/bitlab16/kafka_producer/Consumer.java
@@ -42,13 +42,10 @@ public class Consumer implements Runnable, AutoCloseable {
             while(true) {
                 long timestamp = System.currentTimeMillis();
                 SourceRecord sourceRecord = queue.take();
-                if(sourceRecord.getTime().toInstant().toEpochMilli() < timestamp) {
-                    ProducerRecord<Long, SourceRecord> kafkaRecord = new ProducerRecord<>(TOPIC, timestamp, sourceRecord);
-                    kafkaProducer.send(kafkaRecord);
-                } else {
-
+                if(sourceRecord.getTime().toInstant().toEpochMilli() > timestamp)
                     Thread.sleep(300000);
-                }
+                ProducerRecord<Long, SourceRecord> kafkaRecord = new ProducerRecord<>(TOPIC, timestamp, sourceRecord);
+                kafkaProducer.send(kafkaRecord);
             }
         } catch (InterruptedException ex) {
             System.out.println("Consumer interrotto");
